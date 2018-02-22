ba0598caaba758612770c06c8f96b75bf3b3d91d
Alumno <Alumno@Windows7-PC>
2018-02-22 14:15:13 +0100

    Version1.1 Primera versión de proyecto Holamundo con Git

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -17,6 +17,7 @@ public class Git {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Añadimos un cambio");
     }
     
 }
